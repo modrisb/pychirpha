d3e99b3ff7805378401ade26c167310f777f7019
modrisb <modrisb@apollo.lv>
2026-04-30 20:30:16 +0300

    removed classes.json file to const.py file, added start code

--- src/__init__.py
+++ src/__init__.py
@@ -2,26 +2,18 @@
 
 from __future__ import annotations
 
-import json
 import logging
-from pathlib import Path
 
-import aiofiles
-
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
-
-from .const import CONF_APPLICATION_ID, DOMAIN, GRPCLIENT, MQTTCLIENT
-from .grpc import ChirpGrpc
-from .mqtt import ChirpToHA
+from .const import (
+    DETAILED_LEVEL_NUM,
+    DETAILED_LEVEL_STR,
+)
 
 _LOGGER = logging.getLogger(__name__)
 __version__ = "1.2.0"
 
 # https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility/13638084#13638084
-DETAILED_LEVEL_NUM = 5
-logging.addLevelName(DETAILED_LEVEL_NUM, "DETAIL")
+logging.addLevelName(DETAILED_LEVEL_NUM, DETAILED_LEVEL_STR)
 
 
 def detail(self, message, *args, **kws):
@@ -32,63 +24,3 @@ def detail(self, message, *args, **kws):
 
 logging.Logger.detail = detail  # type: ignore[attr-defined]
 # https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility/13638084#13638084
-
-#  List of platforms to support. There should be a matching .py file for each,
-#  eg <cover.py> and <sensor.py>
-PLATFORMS: list[Platform] = [Platform.BUTTON, Platform.SENSOR]
-
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Chirp from a config entry."""
-    # Store an instance of the "connecting" class that does the work of speaking
-    # with your actual devices.
-    _LOGGER.debug(
-        "async_setup_entry application id %s", entry.data.get(CONF_APPLICATION_ID)
-    )
-    hass.data.setdefault(DOMAIN, {})
-
-    grpc_client = ChirpGrpc(entry.data, __version__)
-    try:
-        async with aiofiles.open(
-            Path(__file__).with_name("classes.json").absolute()
-        ) as file:
-            classes = json.loads(await file.read())
-    except Exception:  # noqa: BLE001
-        classes = None
-        _LOGGER.warning(
-            "File classes.json not loaded, device class defaults might be affected"
-        )
-    mqtt_client = ChirpToHA(entry.data, __version__, classes, grpc_client)
-
-    entry.runtime_data = {
-        GRPCLIENT: grpc_client,
-        MQTTCLIENT: mqtt_client,
-    }
-
-    await hass.async_add_executor_job(mqtt_client._client.loop_start)  # noqa: SLF001
-
-    # This creates each HA object for each platform your device requires.
-    # It's done by calling the `async_setup_entry` function in each platform module.
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    _LOGGER.debug("async_setup_entry completed")
-    return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    # This is called when an entry/configured device is to be removed. The class
-    # needs to unload itself, and remove callbacks. See the classes for further
-    # details
-    _LOGGER.debug("async_unload_entry started")
-
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    _LOGGER.debug("async_unload_entry platforms %s", unload_ok)
-    if unload_ok:
-        if entry.runtime_data:
-            entry.runtime_data[MQTTCLIENT].close()
-            entry.runtime_data[GRPCLIENT].close()
-            entry.runtime_data = None
-    _LOGGER.debug(
-        "async_unload_entry completed, platform unload return code %s", unload_ok
-    )
-    return unload_ok
---
+++ src/start.py
@@ -0,0 +1,105 @@
+"""The Chirpstack LoRaWAN integration - setup."""
+from __future__ import annotations
+__version__ = "1.1.51"
+
+import logging
+import logging.handlers
+import json
+import signal
+import os
+import sys
+from pathlib import Path
+from typing import Final
+import threading
+import traceback
+
+from .grpc import ChirpGrpc
+from .mqtt import ChirpToHA
+from .const import CONF_API_SERVER, CONF_API_PORT, CONF_MQTT_SERVER, CONF_MQTT_PORT, CONF_OPTIONS_LOG_LEVEL
+from .const import DEFAULT_API_SERVER, DEFAULT_API_PORT, DEFAULT_MQTT_PORT, DEFAULT_MQTT_SERVER
+from .const import CONF_MQTT_CHIRPSTACK_PREFIX, DEFAULT_MQTT_CHIRPSTACK_PREFIX, CLASSES
+
+# Date/Time formats
+FORMAT_DATE: Final = "%Y-%m-%d"
+FORMAT_TIME: Final = "%H:%M:%S"
+FORMAT_DATETIME: Final = f"{FORMAT_DATE} {FORMAT_TIME}"
+FMT = ( "%(asctime)s.%(msecs)03d chirpha %(levelname)s %(message)s" )
+
+CONFIGURATION_FILE = '/data/options.json'
+
+_LOGGER = logging.getLogger(__name__)
+
+INTERNAL_CONFIG = {
+  CONF_API_SERVER: DEFAULT_API_SERVER,
+  CONF_API_PORT: DEFAULT_API_PORT,
+  CONF_MQTT_SERVER: DEFAULT_MQTT_SERVER,
+  CONF_MQTT_PORT: DEFAULT_MQTT_PORT,
+  CONF_MQTT_CHIRPSTACK_PREFIX: DEFAULT_MQTT_CHIRPSTACK_PREFIX,
+}
+
+class run_chirp_ha:
+    def __init__(self, configuration_file) -> None:
+        self._grpc_client = None
+        self._mqtt_client = None
+        self._config = None
+        self._configuration_file = configuration_file
+        signal.signal(signal.SIGTERM, self.stop_chirp_ha)
+        threading.excepthook = self.subthread_failed
+
+    def subthread_failed(self, args):
+        _LOGGER.error("Chirp failed(1): %s : %s", args.exc_value, traceback.format_tb(args.exc_traceback))
+        self.close_mqtt_loop()
+
+    def stop_chirp_ha(self, signum, frame):
+        _LOGGER.info("Shutdown requested")
+        self.close_mqtt_loop()
+
+    def close_mqtt_loop(self):
+        if self._mqtt_client:   # close to exit server loop
+            _LOGGER.info("Closing MQTT connection")
+            self._mqtt_client.close()
+            self._mqtt_client = None
+
+    def main(self):
+        logging.basicConfig(level=logging.INFO, format=FMT, datefmt=FORMAT_DATETIME)
+        config = None
+        try:
+            with open(self._configuration_file, 'r') as file:
+                config = json.load(file)
+            print("start.py o", config)
+            config = INTERNAL_CONFIG | config
+            print("start.py u", config)
+            self._config = config
+            _LOGGER.debug("Configuration %s:", self._config)
+            _LOGGER.info("ChirpHA started")
+            try:
+                logging.getLogger().setLevel(config[CONF_OPTIONS_LOG_LEVEL].upper())
+            except Exception as error:  # noqa: F841
+                _LOGGER.warning("Wrong log level specified '%s', assuming 'info'", config[CONF_OPTIONS_LOG_LEVEL])
+                config[CONF_OPTIONS_LOG_LEVEL] = 'info'
+            _LOGGER.debug("Logging level %s", config[CONF_OPTIONS_LOG_LEVEL].upper())
+            _LOGGER.detail("Current directory %s, module directory %s", os.getcwd(), str(Path(__file__).absolute().parent))
+            _LOGGER.detail("Configuration file %s", self._configuration_file)
+            _LOGGER.info("Version %s", __version__)
+            self._grpc_client = ChirpGrpc(config, __version__)
+            self._mqtt_client = ChirpToHA(config, __version__, CLASSES, self._grpc_client)
+            self._mqtt_client._client.loop_forever()
+        except Exception as error:
+            if logging.getLogger().getEffectiveLevel() == logging.DEBUG:
+                _LOGGER.exception("Chirp failed: %s", str(error))
+            else:
+                _LOGGER.error("Chirp failed: %s", str(error))
+        finally:
+            if self._mqtt_client:
+                _LOGGER.info("Closing MQTT connection")
+                self._mqtt_client.close()
+                self._mqtt_client = None
+            if self._grpc_client:
+                _LOGGER.info("Closing gRPC connection")
+                self._grpc_client.close()
+                self._grpc_client = None
+        _LOGGER.info("ChirpHA done")
+
+if __name__=='__main__':
+    run_chirp_ha(sys.argv[1] if len(sys.argv)>1 else CONFIGURATION_FILE).main()
+    _LOGGER.info("Done.")
